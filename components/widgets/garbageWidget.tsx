e651a34be7fb7a7a003b302ea19c9da5d57a3c37
luukdg <luukdegraaf1993@gmail.com>
2026-06-23 09:31:43 +0200

    Changed widget to client polling

--- components/widgets/garbageWidget.tsx
+++ components/widgets/garbageWidget.tsx
@@ -1,12 +1,33 @@
+"use client";
+
 import { Card, CardContent } from "@components/ui/card";
 import { getUpcomingGarbageEvents } from "@/lib/garbage/getGarbage";
 import { formatGarbageDate } from "@/lib/garbage/formatGarbageDate";
 import { Badge } from "@components/ui/badge";
 import { glassCard } from "@/lib/constants/glassCard";
 import { wasteColors } from "@/lib/constants/wasteColors";
+import { useState, useEffect } from "react";
+
+type garbage = {
+  labels: string[];
+  date: string;
+  weekday: string;
+  types: string[];
+};
 
 export function GarbageWidget() {
-  const garbageEvents = getUpcomingGarbageEvents();
+  const [garbageEvents, setGarbageEvents] = useState<garbage[]>([]);
+
+  useEffect(() => {
+    function load() {
+      const event = getUpcomingGarbageEvents();
+      setGarbageEvents(event);
+    }
+
+    load();
+    const interval = setInterval(load, 60 * 60 * 1000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <Card className={`h-full ${glassCard} p-0`}>
